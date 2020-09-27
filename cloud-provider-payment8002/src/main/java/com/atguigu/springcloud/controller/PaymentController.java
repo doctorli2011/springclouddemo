85a2e8bcd2b3c480d6b476f87b187608fe78c105
libo <njsb.good@163.com>
2020-09-27 18:32:31 +0800

    springcloud euraka learning

--- cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -5,8 +5,6 @@ import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @RestController
 @Slf4j
@@ -22,9 +19,6 @@ public class PaymentController {
 
 	@Resource
 	private PaymentService paymentService;
-
-	@Resource
-	private DiscoveryClient discoveryClient;
 
 	@Value("${server.port}")
 	private String serverPort;
@@ -49,19 +43,5 @@ public class PaymentController {
 		} else {
 			return new CommonResult(444, "没有对应记录，查询 ID：" + id, null);
 		}
-	}
-
-	@GetMapping(value = "/payment/discovery")
-	public Object discovery() {
-		final List<String> services = discoveryClient.getServices();
-		for (final String element : services) {
-			log.info("*****element:" + element);
-		}
-
-		final List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
-		for (final ServiceInstance instance : instances) {
-			log.info(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
-		}
-		return this.discoveryClient;
 	}
 }
